e70a230f47d72ea17530ea76a38066a698982c74
juniorsemacento <juniorsemacento@users.noreply.github.com>
2024-08-10 11:27:00 -0400

    Update musthave.sh

--- musthave.sh
+++ musthave.sh
@@ -9,3 +9,18 @@ export nk='-n kube-system'
 export n='-n important-ns' # set this as needed
 export now='--grace-period 0 --force'
 jq -r 'path(..) | map(tostring) | join(".")'
+
+
+set et # expandtab (use space character when tab key used) *
+set ts=2 # tabstop *
+set sw=2 # shiftwidth *
+set sts=2 # softtabstop (Let backspace delete indent **
+set ai # autoindent (Indent at the same level of the previous line)
+set si # smart indent
+set hls # highlightsearch (Highlight search terms)
+set ic # ignorecase when searching in vim
+set bg=dark # Assume a dark background (better color scheme)
+set nu # show line numbers
+syntax on # syntax highlighting
+
+:Wq!
